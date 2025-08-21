b2ed8ee735e806cdad62a75484879bbde0af2173
钦烨 <admibrill@outlook.com>
2025-08-21 08:00:40 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ class CMakeBuild(build_ext):
             f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}{os.sep}",
             f"-DPYTHON_EXECUTABLE={sys.executable}",
             f"-DCMAKE_BUILD_TYPE={cfg}",  # not used on MSVC, but no harm
-            "-C "
+            # "-C "
         ]
         build_args = []
         # Adding CMake arguments set as environment variable
